6f154f9adebc2788d1543724c9c314100a0e6532
xiaguangting <18627924383@163.com>
2019-10-11 16:48:00 +0800

    u

--- DnfDeals/settings.py
+++ DnfDeals/settings.py
@@ -1,16 +1,16 @@
 # DataBase
-HOST = ''
-PORT = ''
-USER = ''
-PASSWORD = ''
-DB = ''
+HOST = '39.105.194.66'
+PORT = '3306'
+USER = 'root'
+PASSWORD = '123456'
+DB = 'dnfdeals'
 
 # Email
 EMAIL_SMTP_SERVER = 'smtp.163.com'
-EMAIL_USERNAME = ''
-EMAIL_PASSWORD = ''
-EMAIL_ADDRESS = ''
-ACCEPT_EMAIL_list = []
+EMAIL_USERNAME = '18627924383'
+EMAIL_PASSWORD = 'abc123'
+EMAIL_ADDRESS = '18627924383@163.com'
+ACCEPT_EMAIL_list = ['18627924383@163.com', '397902881@qq.com']
 
 # Url
 DEALS_URL = 'https://bang.qq.com/app/dnf/acution/index?serverId=24'
